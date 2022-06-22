7f56e69e8f1719bf086f36271b6a8dbab22ae282
Dikshyanta Aryal <adikshyanta456@gmail.com>
2022-06-22 09:02:59 +0545

    createtable array created

--- server/components/sql_queries.js
+++ server/components/sql_queries.js
@@ -8,18 +8,20 @@ let createDatabase = `CREATE DATABASE if not exists ${databaseName}`;
 /**
  * Defining the queries...
  */
-// queries creating table of entities as per the schema
-const companyTable =
-  "create table if not exists production_company(name varchar(50) int primary key auto_increment, title varchar(255)not null default 0)";
-const actorTable = 
-  "create table if not exists actor (actor_name varchar(50) primary key, actor_DOB date)";
-const directorTable = 
-  "create table if not exists director (director_name varchar(50) primary key, director_DOB date)";
-const movieTable = 
-  "create table if not exists movie (title varchar(100) not null primary key,length varchar(20) not null default 0mins, year_of_release int(4), plot_outline varchar(250))";
-// multivalue attribute genre referencing to title of movieTable
-const genreTable = 
-  "create table if not exists Moviegenre (title foreign key, genre varchar(25))";
+const createTable = [
+  "create table if not exists production_company(name varchar(50) primary key,address varchar(50))",
+  "create table if not exists actor (actor_name varchar(50) primary key, actor_DOB date)",
+  "create table if not exists director (director_name varchar(50) primary key, director_DOB date)",
+  "create table if not exists movie (title varchar(100) primary key,prod_company varchar(50),length varchar(20) not null default 0mins, year_of_release int(4), plot_outline varchar(250)),foreign key(prod_company) references production_company(name)",
+  
+  // multivalue attribute genre referencing to title of movieTable
+  "create table if not exists Moviegenre (title foreign key, genre varchar(25))",
+
+  //table for m to n relaltions (acted by )
+  "create table if not exists acts (movie_name varchar(100),actor_name varchar(50),foreign key(movie_name) references movie(movie_name),foreign key(actor_name) references actor(actor_name),role varchar(50) primary key)",
+  "create table if not exists movieQuotes(role varchar(50),quotes varchar(250))"
+]
+
 
 
 
