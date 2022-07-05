d3c48bfb1bae7b121399bd169870b1c35f698a59
Ebraj Grg <gurungebraj22@gmail.com>
2022-07-05 16:53:40 +0545

    client - homepage completed

---
+++ client/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="max-w-[1200px] mx-auto py-2 space-y-2">
+      <h2>MovieHub</h2>
+      <p className="text-gray-400">
+        A site that holds all the details of wonderful movies.
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="flex flex-col min-h-[100vh]">
+        <div className="bg-custom-blue px-5 py-6">
+          <Navbar />
+        </div>
+        <div className="px-5 py-6 sm:py-14 flex-1">
+          <Component {...pageProps} />
+        </div>
+        <div className="bg-custom-blue px-5 py-6">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
