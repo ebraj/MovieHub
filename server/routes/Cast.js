450428e20c466e33fc0e43d9cbe9071de35b733e
Dikshyanta Aryal <65499807+Senor101@users.noreply.github.com>
2022-07-26 14:24:26 +0545

    Genre queries and castdetails (#37)

--- server/routes/Cast.js
+++ server/routes/Cast.js
@@ -4,14 +4,12 @@ const { connection, displayTable } = require("../models");
 const castRouter = express.Router();
 
 castRouter.get("/", (req, res) => {
-  connection.query(displayTable.showCast, (err, results) => {
+  connection.query(displayTable.showCastDetails, (err, results) => {
     if (err) console.log(`Error: ${err.message}`);
     cast = results;
     res.send(cast);
   });
 });
-
-
 
 castRouter.post("/", (req, res) => {});
 module.exports = castRouter;
